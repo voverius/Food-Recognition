76edb9ee615a5bb48de236846909bc10e9a4e699
voverius <xsjadovic@gmail.com>
2022-10-16 16:19:06 +0200

    Optimize training loop

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import models as m
+from python.models import data_augmentation
 import train as t
 import visualization as viz
 import evaluate as ev
@@ -17,13 +18,11 @@ epochs = 30
 # Data Augementation
 train_generator, validation_generator = m.data_augmentation(train_data_dir=train_data_dir, validation_data_dir=validation_data_dir, img_width=img_height, img_height=img_height, batch_size=batch_size)
 
-
 # Load the models
 models = ['ResNet152V2', 'InceptionV3', 'VGG16', 'Xception', 'EfficientNetV2S']
 
-
 for mod in models:
-    model = m.get_model(mod, False, 'imagenet')
+    model = m.get_model(mod, False, None) # Change None to 'imagenet' if you want to use Transfer Learning from ImageNet pre-trained models
     # Fine Tuning
     model = m.model_finetuning(model)
     # Model Training
